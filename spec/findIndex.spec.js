3b5513813c30daa9762d8efc5d8ef95305ac9d73
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 16:51:23 -0500

    Planned test cases for findIndex

--- spec/findIndex.spec.js
+++ spec/findIndex.spec.js
@@ -1,5 +1,12 @@
 describe('findIndex', function() {
   it('should exist', function() {
     expect(_.findIndex).to.exist;
+    //input: array, predicate function
+    //output: number (index), or -1 if not found
+    //typeof predicate should be a function
+    //should return -1 if no items in array pass predicate test
+    //should return index of first item that passes predicate test
+    //if passed a mixed array, predicate function should only handle those items with proper data type
+    //typeof passed in first arg should be an array
   });
 });
