2a4d33a6eb213487bd79bd10cd9bbf09039d371a
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 18:05:28 -0500

    Planned test cases for reduce

--- spec/reduce.spec.js
+++ spec/reduce.spec.js
@@ -1,5 +1,11 @@
 describe('reduce', function() {
   it('should exist', function() {
     expect(_.reduce).to.exist;
+    //input: array or object, predicate function, optional memo parameter of various data type
+    //output: a single value (array, object, number, or string)
+    //if passed [1, 2, 3, 4, 5], sumNumbers, 5 --> should return 19
+    //if passed [1, 2, 3, 4, 5], sumNumbers, no memo --> should return 15
+    //if passed [1, 2, 'chicken', 4, 5], sumNumbers, no memo --> should return NaN? or maybe '3chicken45'?*
+    //if passed {'age': 10, 'heightInCm': 150, 'weight': 100, 'shoeSize': 7}, sumNumbers, no memo --> should return 267?*
   });
 });
