1656fa9e9cf1e9ce405d2c7f3d39e7d6418673fd
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 17:28:33 -0500

    Planned test cases for map

--- spec/map.spec.js
+++ spec/map.spec.js
@@ -1,5 +1,11 @@
 describe('map', function() {
   it('should exist', function() {
     expect(_.map).to.exist;
+    //input: array or object and a predicate function
+    //output: array or object
+    //output of function should never equal input collection
+    //length of output collection should equal input collection
+    //typeof input collection should be object
+    //typeof passed in iteratee should be function
   });
 });
