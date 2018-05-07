e5cdbaf4f07ff06c7e8dc5bafec2b2d3d064af1b
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 17:03:21 -0500

    Planned test cases for filter

--- spec/filter.spec.js
+++ spec/filter.spec.js
@@ -1,5 +1,10 @@
 describe('filter', function() {
   it('should exist', function() {
     expect(_.filter).to.exist;
+    //input: array or object and a function (predicate)
+    //output: array or object
+    //should return array or object of values that pass the predicate function test from original input array/object
+    //should return an empty array/object if no values pass predicate test
+    //if input array/object is empty, return empty array/object
   });
 });
