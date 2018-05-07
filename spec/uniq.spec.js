a371f2d78cdf3e78c2d0b30ae0715cd9f5540ab4
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 17:19:48 -0500

    Planned test cases for uniq

--- spec/uniq.spec.js
+++ spec/uniq.spec.js
@@ -1,5 +1,13 @@
 describe('uniq', function() {
   it('should exist', function() {
     expect(_.uniq).to.exist;
+    //input: array, optional: boolean test, optional: function
+    //output: array with no duplicate values
+    //assumptions: positive and negative numbers are unique
+    //if passed [1, 2, 2, 3, 3, 4], should return [1, 2, 3, 4]
+    //if passed [0, 3, 'string', true], should return [0, 3, 'string', true]
+    //if passed ([1, 2, 2, 3, 4, 5], isEven), should return [2, 4]
+    //if passed [-1, 1, -2, 2], should return [-1, 1, -2, 2]
+    //typeof isSorted, if passed in, should be boolean
   });
 });
