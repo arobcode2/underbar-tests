9e51515c013edc70a67f9ec7d35c67a40710814f
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 13:41:00 -0500

    Planned test cases for each

--- spec/each.spec.js
+++ spec/each.spec.js
@@ -1,5 +1,17 @@
 describe('each', function() {
   it('should exist', function() {
     expect(_.each).to.exist;
+    //inputs: 1 array or object (list), 1 function (value, key, list)
+    //outputs: array or object matching the input datatype
+    //constaints: cannot pass in objects with a numeric length property
+    //regardless if a context is passed in or not, the function returns the list
+    //if context object is passed in, its length should equal the length of the list passed in
+    //if there is no iteratee, return the list
+    //if context is passed in but is not an object, function should return the list but context should be undefined*?
+    //typeof iteratee should always be a function- if iteratee is not a function, the each func would return undefined
+    //if the iteratee is passed improper datatype from list, it should not affect the return list
+    //if list is empty return empty list
+    //if list is undefined return undefined
+    //check for the typeof list and if list is not an array or object, return undefined
   });
 });
