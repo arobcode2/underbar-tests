b2b13a6d9d7e387bfc2dd1602c6fb0659def9d14
Andrew Rob <andrew-c-rob@live.com>
2018-05-07 17:07:59 -0500

    Planned test cases for reject

--- spec/reject.spec.js
+++ spec/reject.spec.js
@@ -1,5 +1,10 @@
 describe('reject', function() {
   it('should exist', function() {
     expect(_.reject).to.exist;
+    //input: array or object and a function (predicate)
+    //output: array or object
+    //should return array or object of values that do not pass the predicate function test from original input array/object
+    //should return an empty array/object if all values pass predicate test
+    //if input array/object is empty, return empty array/object
   });
 });
