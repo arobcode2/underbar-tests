bc2f68494105794b1b3a3d60742d2050299578f0
Andrew Rob <andrew-c-rob@live.com>
2018-05-08 16:17:04 -0500

    Wrote test cases for uniq Please enter the commit message for your changes. Lines starting

--- spec/uniq.spec.js
+++ spec/uniq.spec.js
@@ -1,13 +1,25 @@
 describe('uniq', function() {
+  //input: array, optional: boolean test, optional: function
+  //output: array with no duplicate values
+  //assumptions: positive and negative numbers are unique
   it('should exist', function() {
     expect(_.uniq).to.exist;
-    //input: array, optional: boolean test, optional: function
-    //output: array with no duplicate values
-    //assumptions: positive and negative numbers are unique
-    //if passed [1, 2, 2, 3, 3, 4], should return [1, 2, 3, 4]
-    //if passed [0, 3, 'string', true], should return [0, 3, 'string', true]
-    //if passed ([1, 2, 2, 3, 4, 5], isEven), should return [2, 4]
-    //if passed [-1, 1, -2, 2], should return [-1, 1, -2, 2]
-    //typeof isSorted, if passed in, should be boolean
+  });
+  //if passed [1, 2, 2, 3, 3, 4], should return [1, 2, 3, 4]
+  it('should remove all duplicates from passed in array', function() {
+    expect(_.uniq([1, 2, 2, 3, 3, 4])).to.deep.equal([1, 2, 3, 4]);
+  });
+  //if passed [0, 3, '3', true], should return [0, 3, '3', true]
+  it('should treat different datatypes as unique values', function() {
+    expect(_.uniq([0, 3, '3', true])).to.deep.equal([0, 3, '3', true]);
+  });
+  //if passed ([1, 2, 2, 3, 4, 5, 'str'], multiplyBy0, should return [1, 'str']
+  it('should compute unique items based on iteratee transformation', function() {
+    let multiplyBy0 = function(num) {return num * 0;};
+    expect(_.uniq([1, 2, 2, 3, 4, 5, 'str'], multiplyBy0)).to.deep.equal([1, 'str']);
+  });
+  //if passed [-1, 1, -2, 2], should return [-1, 1, -2, 2]
+  it('should treat positive and negative integers as unique numbers', function() {
+    expect(_.uniq([-1, 1, -2, 2])).to.deep.equal([-1, 1, -2, 2]);
   });
 });
