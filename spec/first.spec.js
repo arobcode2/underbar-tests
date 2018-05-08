256c857b6dbb909b2ab580b2f2d9f892e6d39eee
Andrew Rob <andrew-c-rob@live.com>
2018-05-08 12:15:53 -0500

    Wrote test cases for first

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -1,13 +1,31 @@
 describe('first', function() {
+  //inputs: 1 array and a number
+  //outputs: 1 array or 1 value of datatype depending on number of arguments passed in
   it('should exist', function() {
     expect(_.first).to.exist;
-    //inputs: 1 array and a number
-    //outputs: 1 array or 1 value of datatype depending on number of arguments passed in
-    //the passed in array can contain any datatypes... all numbers, all strings, all booleans, mixede arrays, nested arrays, etc.
-    //if just an array is passed in then return first element of array.
-    //The length of output array should equal n (number passed in) if a number is passed in
-    //if the passed in array is empty, whether of not n is passed in, it returns undefined
-    //if n is not a positive integer if passed in, the function should return undefined
-    //if n is larger than the length of the array, return whole array
+  });
+  //if a mixed array and n (number) is passed in return the first n elements in array
+  it('should return first n elements of array if an array and n (number) are passed in', function() {
+    expect(_.first(['apple', [1, 2, 3], 56, true], 3)).to.deep.equal(['apple', [1, 2, 3], 56]);
+  });
+  //if just an array is passed in then return first element of array.
+  it('should return first element of array if only an array is passed in', function() {
+    expect(_.first(['apple', [1, 2, 3], 56, true])).to.equal('apple');
+  });
+  //The length of output array should equal n (number passed in) if a number is passed in
+  it('should return an array of length equal to n if given', function() {
+    expect(_.first(['apple', [1, 2, 3], 56, true], 3).length).to.equal(3);
+  });
+  //if the passed in array is empty, whether of not n is passed in, it returns an empty array
+  it('should return an empty array if given an empty array', function() {
+    expect(_.first([], 2)).to.deep.equal([]);
+  });
+  //if n is not a positive integer if passed in, the function should return empty array
+  it('should return empty array if n is non-positive', function() {
+    expect(_.first([1, 2, 3, 4, 5], -2)).to.deep.equal([]);
+  }); 
+  //if n is larger than the length of the array, return whole array
+  it('should return the entire array if length is less than n', function() {
+    expect(_.first([1, 2, 3, 4, 5], 6)).to.deep.equal([1, 2, 3, 4, 5]);
   });
 });
